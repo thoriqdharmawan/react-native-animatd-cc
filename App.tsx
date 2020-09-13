c3f400103c9f24e0348c885829ec2a83a0bf3392
thoriqdharmawan <thoriqd21@gmail.com>
2020-09-13 19:38:26 +0700

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const value = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0];
+
+  const moveBall = () => {
+    Animated.timing(value, {
+      toValue: { x: 100, y: 100 },
+      duration: 1000,
+      useNativeDriver: false,
+    }).start();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Animated.View style={value.getLayout()} >
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 100 / 2,
+            backgroundColor: "red",
+          }}
+        />
+      </Animated.View>
+      <TouchableOpacity onPress={moveBall}>
+        <Text>Click me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
